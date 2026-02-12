8c902d11cf48c5a34ecb6813cc4c384016ba12c7
Nima Shayanfar <nshayanfar@gmail.com>
2026-02-12 18:50:05 +0330

    [Changed] moved imports to make startup faster

--- htr_seg_select/selector/admin.py
+++ htr_seg_select/selector/admin.py
@@ -1,21 +1,22 @@
+import os
+from os.path import join
+from pathlib import Path
+
+from django import forms
+from django.conf import settings
 from django.contrib import admin
+from django.db import models
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.html import format_html
-from os.path import join
-from django.conf import settings
-from django import forms
-from django.db import models
-from kraken import binarization, blla, pageseg
-from kraken.lib import vgsl
-from PIL import Image
-from pathlib import Path
-from .models import Document, LineSegment
-from .segmentation import extract_polygons
 from django.utils.translation import gettext_lazy as _
-import os
+
+from .models import Document, LineSegment
 
 
 def segment_document(modeladmin, request, queryset):
+    from kraken.lib import vgsl
+
     model_muharaf = vgsl.TorchVGSLModel.load_model("muharaf_seg_best.mlmodel")
     model_blla = vgsl.TorchVGSLModel.load_model("blla.mlmodel")
     for doc in queryset:
@@ -39,46 +40,61 @@ class HasLinesegmentsFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         val = self.value()
         if val == "yes":
-            return queryset.filter(id__in=[doc.pk for doc in queryset if doc.has_linesegments])
+            return queryset.filter(
+                id__in=[doc.pk for doc in queryset if doc.has_linesegments]
+            )
         elif val == "no":
-            return queryset.filter(id__in=[doc.pk for doc in queryset if not doc.has_linesegments])
+            return queryset.filter(
+                id__in=[doc.pk for doc in queryset if not doc.has_linesegments]
+            )
         return queryset
+
 
 class IsTranscribedFilter(admin.SimpleListFilter):
-    title = _('رونوشت شده')
-    parameter_name = 'is_transcribed'
+    title = _("رونوشت شده")
+    parameter_name = "is_transcribed"
 
     def lookups(self, request, model_admin):
         return (
-            ('yes', _('بله')),
-            ('no', _('خیر')),
+            ("yes", _("بله")),
+            ("no", _("خیر")),
         )
 
     def queryset(self, request, queryset):
         val = self.value()
-        if val == 'yes':
-            return queryset.filter(id__in=[doc.pk for doc in queryset if doc.is_transcribed])
-        elif val == 'no':
-            return queryset.filter(id__in=[doc.pk for doc in queryset if not doc.is_transcribed])
+        if val == "yes":
+            return queryset.filter(
+                id__in=[doc.pk for doc in queryset if doc.is_transcribed]
+            )
+        elif val == "no":
+            return queryset.filter(
+                id__in=[doc.pk for doc in queryset if not doc.is_transcribed]
+            )
         return queryset
+
 
 class IsVerifiedFilter(admin.SimpleListFilter):
-    title = _('تایید شده')
-    parameter_name = 'is_verified'
+    title = _("تایید شده")
+    parameter_name = "is_verified"
 
     def lookups(self, request, model_admin):
         return (
-            ('yes', _('بله')),
-            ('no', _('خیر')),
+            ("yes", _("بله")),
+            ("no", _("خیر")),
         )
 
     def queryset(self, request, queryset):
         val = self.value()
-        if val == 'yes':
-            return queryset.filter(id__in=[doc.pk for doc in queryset if doc.is_verified])
-        elif val == 'no':
-            return queryset.filter(id__in=[doc.pk for doc in queryset if not doc.is_verified])
+        if val == "yes":
+            return queryset.filter(
+                id__in=[doc.pk for doc in queryset if doc.is_verified]
+            )
+        elif val == "no":
+            return queryset.filter(
+                id__in=[doc.pk for doc in queryset if not doc.is_verified]
+            )
         return queryset
+
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
@@ -94,7 +110,7 @@ class DocumentAdmin(admin.ModelAdmin):
         "is_verified",
     ]
     actions = [segment_document]
-    list_filter=[HasLinesegmentsFilter, IsTranscribedFilter, IsVerifiedFilter]
+    list_filter = [HasLinesegmentsFilter, IsTranscribedFilter, IsVerifiedFilter]
 
     def compare_link(self, obj):
         if "_" in obj.name:
@@ -147,7 +163,6 @@ class DocumentAdmin(admin.ModelAdmin):
 
     def _cleanup_document_files(self, obj):
         import glob
-        from django.conf import settings
         import shutil
 
         base_name, _ = os.path.splitext(os.path.basename(obj.file.name))
@@ -180,23 +195,33 @@ class DocumentAdmin(admin.ModelAdmin):
 
     def has_linesegments(self, obj):
         return obj.linesegments.exists()
+
     has_linesegments.boolean = True
     has_linesegments.short_description = _("دارای تکه خط")
 
     def is_transcribed(self, obj):
         return obj.is_transcribed
+
     is_transcribed.boolean = True
     is_transcribed.short_description = _("رونوشت شده")
 
     def is_verified(self, obj):
         return obj.is_verified
+
     is_verified.boolean = True
     is_verified.short_description = _("تایید شده")
 
 
 @admin.register(LineSegment)
 class LineSegmentAdmin(admin.ModelAdmin):
-    list_display = ["id", "document", "order", "transcribed", "verified", "document_file_link"]
+    list_display = [
+        "id",
+        "document",
+        "order",
+        "transcribed",
+        "verified",
+        "document_file_link",
+    ]
     list_filter = ["transcribed", "verified"]
     readonly_fields = ["image_tag"]
     ordering = ["document", "transcribed", "order"]
@@ -223,6 +248,68 @@ class LineSegmentAdmin(admin.ModelAdmin):
         ),
     )
 
+    change_form_template = "admin/selector/linesegment/change_form.html"
+
+    def get_queryset(self, request):
+        # Store request GET params for use in context
+        qs = super().get_queryset(request)
+        self._list_filters = request.GET.urlencode()
+        return qs
+
+    def get_next_untranscribed(self, obj, request):
+        """
+        Returns the next non-transcribed LineSegment queryset, filtered like current changelist (if possible).
+        """
+        filters = {}
+        filter_args = []
+        # Try to preserve filters - supports document id, etc.
+        if "document__id__exact" in request.GET:
+            filters["document__id"] = request.GET["document__id__exact"]
+        if hasattr(self, "get_preserved_filters"):
+            # Use preserved filters from request, if present
+            preserved = self.get_preserved_filters(request)
+            if preserved and request.GET.get("_changelist_filters"):
+                filter_args.append(request.GET.get("_changelist_filters").split("="))
+        if len(filter_args) or len(filters):
+            qs = LineSegment.objects.filter(*filter_args, **filters)
+        else:
+            qs = LineSegment.objects.none()
+        next_segment = qs.filter(transcribed=False).order_by("id").first()
+        return next_segment
+
+    def has_next_untranscribed(self, obj, request):
+        next_seg = self.get_next_untranscribed(obj, request)
+        return next_seg is not None
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        obj = kwargs.get("obj")
+        # context['has_next_untranscribed'] for template logic
+        context["has_next_untranscribed"] = (
+            self.has_next_untranscribed(obj, request) if obj else False
+        )
+        return super().render_change_form(request, context, *args, **kwargs)
+
+    def response_change(self, request, obj):
+        if "save_and_next" in request.POST:
+            next_segment = self.get_next_untranscribed(obj, request)
+            if next_segment:
+                # Preserve filters/query params
+                params = request.GET.urlencode()
+                url = reverse(
+                    "admin:selector_linesegment_change",
+                    args=[next_segment.pk],
+                )
+                if params:
+                    url = f"{url}?{params}"
+                return HttpResponseRedirect(url)
+            else:
+                self.message_user(
+                    request, _("No more non-transcribed line segments."), level="info"
+                )
+                # Fallback to default; stay on current page
+                return super().response_change(request, obj)
+        return super().response_change(request, obj)
+
     def image_tag(self, obj):
         if obj.file:
             return format_html(
@@ -236,13 +323,21 @@ class LineSegmentAdmin(admin.ModelAdmin):
     def document_file_link(self, obj):
         if obj.document and obj.document.file:
             url = obj.document.file.url
-            return format_html('<a href="{}" target="_blank">{}</a>', url, _("گشودن تصویر"))
+            return format_html(
+                '<a href="{}" target="_blank">{}</a>', url, _("گشودن تصویر")
+            )
         return "-"
+
     document_file_link.short_description = _("تصویر سند")
-    document_file_link.admin_order_field = 'document'
+    document_file_link.admin_order_field = "document"
 
 
 def extract_lines_muharaf(im_path, save_prefix: str, doc_name: str, model, padding=10):
+    from kraken import blla
+    from PIL import Image
+
+    from .segmentation import extract_polygons
+
     base_name = os.path.basename(im_path)
     _, ext = os.path.splitext(base_name)
     ext = ext.lstrip(".")
@@ -263,6 +358,11 @@ def extract_lines_muharaf(im_path, save_prefix: str, doc_name: str, model, paddi
 
 
 def extract_lines_blla(im_path, save_prefix: str, doc_name: str, model, padding=10):
+    from kraken import blla
+    from PIL import Image
+
+    from .segmentation import extract_polygons
+
     base_name = os.path.basename(im_path)
     _, ext = os.path.splitext(base_name)
     ext = ext.lstrip(".")
@@ -280,6 +380,11 @@ def extract_lines_blla(im_path, save_prefix: str, doc_name: str, model, padding=
 
 
 def extract_lines_bbox(im_path, save_prefix: str, doc_name: str):
+    from kraken import binarization, pageseg
+    from PIL import Image
+
+    from .segmentation import extract_polygons
+
     # load page image
     im = Image.open(im_path)
 
